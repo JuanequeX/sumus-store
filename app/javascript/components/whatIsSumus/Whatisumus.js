a939aa833fdbbfb4cbf856bb6d197263998ad0f5
JuanequeX <ramon.g.uz37@gmail.com>
2021-07-06 15:01:06 -0500

    Create what is sumus section

---
+++ app/javascript/components/whatIsSumus/Whatisumus.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import whatissumus from './whatissumus.scss';
+
+const What = () => {
+  return (
+    <section className='container-info'>
+      <div className = 'container-info__box'>
+        <h5 className='container-info__title'>¿Que es sumus?</h5>
+        <p className='container-info__subtitle'>Sumus ha sido creado bajo una identidad disyuntiva que busca confeccionar prendas que transmitan una disociación visual.</p>
+      </div>
+    </section>
+   );
+}
+
+export default What;
