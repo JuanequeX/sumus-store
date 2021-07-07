1b465237c6dcf980797175a2b3d68fb5f5e29dbe
JuanequeX <ramon.g.uz37@gmail.com>
2021-07-07 16:20:48 -0500

    Creates Product Section

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './header/Header';
 import Hero from './hero/Hero';
 import Whatissumus from './whatIsSumus/Whatisumus';
+import Products from './products/Products';
 import app from './app.scss';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <Hero />
       <Whatissumus />
+      <Products />
     </>
   )
 }
